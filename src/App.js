2d829c5080dc35305c436fbdbd7433b1c50a119c
Jimmy Kharpatoe <jimmy.kharpatoe@gmail.com>
2022-01-18 00:59:12 -0300

    Navigation ++

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch} from 'react-router-dom';
 import './App.css';
 
+
+const HatsPage=()=>(
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+//voordeel van switch is dat je niet per ongeluk gewoon meerdere routes zal renderen
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+
+      <Switch>
+     <Route exact path='/' component={HomePage} />
+     <Route path='/hats' component={HatsPage} />
+     </Switch>
+      
     </div>
   );
 }
